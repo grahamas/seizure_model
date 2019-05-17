da3237b58469619503f81a785372a4de69c13392
Graham Smith <grahamas@gmail.com>
2019-05-17 15:13:50 -0500

    [wip] added sbatch scripts

---
+++ scripts/based_on_example.jl
@@ -0,0 +1,104 @@
+using DrWatson
+quickactivate(@__DIR__, "seizure_model")
+
+using Simulation73, TravelingWaveSimulations
+using BSON
+using Dates
+using ArgParse
+using Lazy
+
+arg_settings = ArgParseSettings()
+@add_arg_table arg_settings begin
+    "--data-root", "-d"
+        help = "Location in which to store output data"
+        default = datadir()
+    "--example-name"
+        help = "Name of example defined examples.jl"
+    "--modifications-cases"
+        nargs = '*'
+        help = "Name of file specifying dict of modifications"
+    "--plotspec-case"
+        help = "Name of file specifying plots"
+    "--no-save-raw"
+        help = "Don't save raw simulation"
+        action = :store_true
+end
+args = parse_args(ARGS, arg_settings)
+@show args
+data_root = args["data-root"]
+example_name = args["example-name"]
+modifications_cases = args["modifications-cases"]
+plotspec_case = args["plotspec-case"]
+
+ENV["GKSwstype"] = "100" # For headless plotting (on server)
+ENV["MPLBACKEND"]="Agg"
+using Plots
+pyplot()
+
+function read_modification_file(filename::AbstractString)
+    include(joinpath(scriptdir(), "modifications", "$(filename).jl"))
+    return modifications
+end
+
+parse_range(start, stop) = parse(Float64, start):parse(Float64, stop)
+parse_range(start, step, stop) = parse(Float64, start):parse(Float64, step):parse(Float64, stop)
+
+function parse_modification(str::AbstractString)
+    if occursin("=", str)
+        name_str, value_str = split(str, "=")
+        if occursin(":", value_str)
+            return [Dict(Symbol(name_str) => val) for val in parse_range(split(value_str,":")...)]
+        else
+            return Dict(Symbol(name_str) => parse(Float64, value_str))
+        end
+    else
+        return read_modification_file(str)
+    end
+end
+
+must_be_list(x::AbstractArray) = x
+must_be_list(x) = [x]
+
+function parse_modifications_array(modification_strs::AbstractArray)
+    parsed_modifications = @> modification_strs begin
+        parse_modification.()
+        must_be_list.()
+    end
+    modification_cases = Iterators.product(parsed_modifications...)
+    modification_cases = map((case) -> merge(case...), modification_cases)
+    return modification_cases
+end
+
+if modifications_cases != []
+    modifications = parse_modifications_array(modifications_cases)
+    modifications_prefix = """$(join(sort(modifications_cases), ";"))_"""
+else
+    modifications = [Dict()]
+    modifications_prefix = ""
+end
+
+if plotspec_case != []
+    plotspec_path = joinpath(scriptdir(), "plotspecs", "$(plotspec_case).jl")
+    include(joinpath(scriptdir(), "plotspecs", plotspec_path)) # defines plotspecs
+    plots_path = joinpath(plotsdir(), example_name)
+    plots_path = joinpath(plots_path, "$(modifications_prefix)$(plotspec_case)_$(Dates.now())_$(current_commit())")
+    mkpath(plots_path)
+else
+    plotspecs = []
+end
+
+if !args["no-save-raw"]
+    sim_output_path = joinpath(data_root, "sim", example_name, "$(modifications_prefix)$(Dates.now())_$(current_commit())")
+    mkpath(sim_output_path)
+end
+
+for modification in modifications
+    simulation = get_example(example_name)(; modification...)
+    execution = execute(simulation)
+    mod_name = savename(modification; allowedtypes=(Real,String,Symbol,AbstractArray), connector=";")
+    if !args["no-save-raw"]
+        execution_dict = @dict execution
+        @tagsave("$(joinpath(sim_output_path, mod_name)).bson", execution_dict, true)
+    end
+    plot_and_save.(plotspecs, Ref(execution), plots_path, mod_name)
+end
---
+++ scripts/master_sbatch.jl
@@ -0,0 +1,83 @@
+using DrWatson
+quickactivate(@__DIR__, "seizure_model")
+using ArgParse
+
+
+function parse_commandline(args)
+    arg_settings = ArgParseSettings(
+                                    autofix_names = false,
+                                    )
+    @add_arg_table arg_settings begin
+        "--ntasks"
+            help = "Total number of tasks (cpus)"
+            arg_type = Int
+            default = 1
+        "--mem-per-cpu"
+            help = "RAM per CPU (MB)"
+            arg_type = Int
+            default = 1000
+        "--time"
+            help = "Job duration (seconds)"
+            default = "30:00"
+        "--partition"
+            help = "Name of partition to run job on"
+            default = "debug"
+        "--project-root"
+            help = "Name of code directory"
+            default = projectdir()
+        "--data-root"
+            help = "Name of data output directory"
+            default = datadir()
+        "--mail-user"
+            help = "Email target for SLURM status updates"
+        "--mail-type"
+            help = "Types of mail to send user"
+        "--base-example", "-b"
+            help = "Name of predefined example to use as base"
+        "--script-name", "-s"
+            help = "Name of script to run (path in script dir)"
+            default = "based_on_example.jl"
+        "--plotspec-case"
+            help = "Case specifying plots"
+        "--modifications-case"
+            nargs = '*'
+            help = "Case specifying base model modifications"
+        "--no-save-raw"
+            help = "Don't save the raw simulation"
+            action = :store_true
+    end
+    return parse_args(args, arg_settings; as_symbols = false)
+end
+
+function sbatch_script(ARGS)
+    args = parse_commandline(ARGS)
+    project_root = args["project-root"]
+    base_example = args["base-example"]
+    script_name = args["script-name"]
+    sbatch_arg_names = ["ntasks", "mem-per-cpu", "time", "partition", "workdir", "mail-user",  "mail-type"]
+    script_output_dir = joinpath(project_root, "_slurm", "output", base_example)
+    mkpath(script_output_dir)
+    sbatch_args = ["--$name=$val" for (name, val) in args if (name in sbatch_arg_names && val != nothing)]
+    sbatch_args = [sbatch_args..., """--output=$(joinpath(script_output_dir, "%j.stdout"))"""]
+    sbatch_args = [sbatch_args..., """--error=$(joinpath(script_output_dir, "%j.stderr"))"""]
+    sbatch_args = [sbatch_args..., "--job-name=$(base_example)"]
+
+    script_arg_names = ["modifications-case", "plotspec-case", "data-root"]
+    script_args = ["--$name=$val" for (name, val) in args if (name in script_arg_names && val != nothing)]
+    script_args = [script_args..., """--example-name=$(args["base-example"])"""]
+    if args["no-save-raw"]
+        script_args = [script_args..., "--no-save-raw"]
+    end
+
+    script_path = joinpath(scriptdir(), script_name)
+
+    sbatch_script = """#!/bin/bash
+    cd $(project_root)
+    julia-nightly $(script_path) $(join(script_args, " "))
+    /usr/bin/mail -s \${SLURM_JOB_NAME} $(args["mail-user"]) < $(joinpath(script_output_dir, "\${SLURM_JOB_ID}.stderr"))
+    """
+
+    run(pipeline(`echo $sbatch_script`, `sbatch $sbatch_args`))
+end
+
+sbatch_script(ARGS)
